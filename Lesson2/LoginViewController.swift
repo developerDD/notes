0526ad93d061de8d5c26508ead1d095ebcf9ad0e
dimaderevanko <developerDD@ukr.net>
2022-03-21 21:52:31 +0200

    додано перехід)

--- Lesson2/LoginViewController.swift
+++ Lesson2/LoginViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -24,17 +24,19 @@ class LoginViewController: UIViewController {
                 print("facebook ID \(identifier)")
             }
         }
+//        нема логування на фейсбука
+        dismiss(animated: true, completion: nil)
     }
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
